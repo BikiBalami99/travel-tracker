21d23ce517095e2c51ed00441c0e1cd0881e2c8e
Biki Balami <bikibalami99@gmail.com>
2024-12-27 00:15:37 +0900

    Implement GET and POST request

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 import express from "express";
-import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+const db = new pg.Client({
+  user: "postgres",
+  password: "Kx-tsc2ciddKx-tsc2cidd",
+  database: "world",
+  host: "localhost",
+  port: 5432,
+});
+db.connect();
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+  const result = await db.query("SELECT country_code FROM visited_countries");
+
+  const countries = result.rows.map((country) => country.country_code);
+  res.render("index.ejs", { countries: countries, total: countries.length });
+});
+
+app.post("/add", async (req, res) => {
+  const userInput = req.body.country.toLowerCase();
+  const countriesList = await db.query(
+    "SELECT country_code, country_name FROM countries"
+  );
+
+  const theCountry = countriesList.rows.find(
+    (country) => country.country_name.toLowerCase() === userInput
+  );
+  const countryCode = theCountry ? theCountry.country_code : null;
+
+  if (countryCode) {
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+  }
+  res.redirect("/");
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log("Server running at port " + port);
 });
